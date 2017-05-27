b291870a4081d9a0f247038aa72a4fbbbd79eb7a
Ian <itjp89@gmail.com>
2017-05-27 19:43:55 -0400

    Added DeathTile to DeathEvent

--- Flax/src/com/flax/api/event/DeathEvent.java
+++ Flax/src/com/flax/api/event/DeathEvent.java
@@ -1,9 +1,19 @@
 package com.flax.api.event;
 
+import org.dreambot.api.methods.map.Tile;
+
 public class DeathEvent extends GameEvent {
 
-	public DeathEvent() {
+	private Tile death_tile = null;
+	
+	public DeathEvent(Tile player_position) {
 		super(GameEvent.EVENT_DEATH);
+		
+		death_tile = player_position;
+	}
+	
+	public Tile getDeathTile() {
+		return death_tile;
 	}
 
 }
