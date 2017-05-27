464861f11e54a63a5a587fd39d3e5f9c5b612bc9
Ian <itjp89@gmail.com>
2017-05-27 02:07:46 -0400

    Fixed ScaleInt issues

--- Flax/src/com/flax/api/utils/MathUtils.java
+++ Flax/src/com/flax/api/utils/MathUtils.java
@@ -13,8 +13,8 @@ public class MathUtils {
     }
 	
 	public static int scaleInt(final int in_value, final int in_min, final int in_max, final int out_min, final int out_max) {
-		return (((out_max - out_min) * (in_value - in_min) / (in_max - in_min)) + out_max);
-	}
+        return (int) (((out_max - out_min) * (in_value - in_min) / (in_max - in_min)) + out_min);
+    }
 	
 	public static double caculatePercent(int value, int max) {
 		return (double) (value * 100.0f) / max;
